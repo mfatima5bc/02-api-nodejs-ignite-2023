354de5fee33170a3e2ee84ef327500d4ecbab495
Maria de Fátima <maria.defatimabarbosacardoso@gmail.com>
2023-02-23 14:28:38 -0300

    test: :sparkles: add vistest setup

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+
+import { transactionsRoutes } from './routes/transactions'
+
+export const app = fastify()
+
+app.register(cookie)
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
+
+app.addHook('preHandler', async (req, replay) => {
+  console.log('global')
+})
